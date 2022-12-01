2cd0150a8f67a940f5c15f55cca67159c94f3516
Stefan Vassilev <stefanvassilev1@gmail.com>
2022-12-01 15:21:52 +0200

    Complete day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
+import java.util.Arrays
+import java.util.ListResourceBundle
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
 }
+
+fun part1(input: List<String>): Int {
+    return getElvesFood(input)
+            .map { it.reduce { acc, next -> acc + next } }
+            .max();
+}
+
+fun part2(input: List<String>): Int {
+    val sortedList = getElvesFood(input)
+            .map { it.reduce { acc, next -> acc + next } }
+            .sorted()
+
+    return sortedList.subList(sortedList.size - 3, sortedList.size).reduce{acc, next -> acc + next}
+}
+
+private fun getElvesFood(input: List<String>): ArrayList<List<Int>> {
+    val elvesFood = ArrayList<List<Int>>()
+
+    var curFood = ArrayList<Int>()
+    for (line in input) {
+        if (line == "") {
+            elvesFood.add(curFood);
+            curFood = ArrayList();
+        } else {
+            curFood.add(Integer.parseInt(line))
+        }
+    }
+    return elvesFood
+}
+
+
