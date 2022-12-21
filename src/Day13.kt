5f7cf1c0c70c392af17beb471986be61f29bd414
Stefan Vassilev <stefanvassilev1@gmail.com>
2022-12-21 11:19:37 +0200

    Complete day13 part 1ˆ

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,7 +1,7 @@
 import java.util.*
 
 fun main() {
-    val input = readInput("Day13-sample")
+    val input = readInput("Day13")
     println(day13Part1(input))
     println(day13Part2(input))
 
@@ -37,34 +37,35 @@ fun day13Part1(input: List<String>): Int {
     }
     pairs.add(Pair(tmp[0], tmp[1]))
 
-    val indeciesOfPacketsInCorrectOrder = ArrayList<Int>()
+    var sumOfIndexes = 0
     var curPairIndex = 1
     for (pair in pairs) {
-        val packet1 = pair.first
-        val el1 = EmbeddedList()
-        getListElem(el1, packet1)
-//        println("packet 1 is: ${el1}")
+        val el1 = getEmbeddedList(pair.first)
+        val el2 = getEmbeddedList(pair.second)
 
-        val packet2 = pair.second
-        val el2 = EmbeddedList()
-        getListElem(el2, packet2)
-//        println("packet 2 is: ${el2}")
-        if (pairInRightOrder(el1, el2)) {
+        println("Input:\nData1: ${el1.list[0]}\nData2: ${el2.list[0]}")
+        if (pairInRightOrder(el1.list[0], el2.list[0]) > 0) {
             println("correct order for pair: $curPairIndex")
-            indeciesOfPacketsInCorrectOrder.add(curPairIndex)
+            sumOfIndexes += curPairIndex
         } else {
             println("NOT correct order for pair: $curPairIndex")
         }
         curPairIndex++
     }
 
-    return indeciesOfPacketsInCorrectOrder.sum()
+    return sumOfIndexes
 }
 
-fun pairInRightOrder(data1: ListElem, data2: ListElem): Boolean {
-    println("Input: Data1: $data1, Data2: $data2")
+private fun getEmbeddedList(str: String): EmbeddedList {
+    val el1 = EmbeddedList()
+    getListElem(el1, str)
+    return el1
+}
+
+fun pairInRightOrder(data1: ListElem, data2: ListElem): Int {
+    println("*** :\nData1: ${data1}\nData2: $data2")
     if (data1 is IntHolder && data2 is IntHolder) {
-        return data1.int <= data2.int
+        return data2.int - data1.int
     }
     if (data1 is IntHolder && data2 is EmbeddedList) {
         return pairInRightOrder(EmbeddedList(ArrayList(listOf(data1))), data2)
@@ -78,44 +79,20 @@ fun pairInRightOrder(data1: ListElem, data2: ListElem): Boolean {
     val el1Iter = el1.list.iterator()
     val el2Iter = el2.list.iterator()
 
-    if (isListOfIntHolder(el1) && isListOfIntHolder(el2)) {
-        while (el1Iter.hasNext()) {
-            if (!el2Iter.hasNext()) {
-                return false
-            }
-            val item1 = el1Iter.next() as IntHolder
-            val item2 = el2Iter.next() as IntHolder
-            if (item1.int < item2.int) {
-                return true
-            }
-            if (item1.int > item2.int) {
-                return false
-            }
-        }
-        return true
-    }
-
     while (el1Iter.hasNext()) {
         if (el1Iter.hasNext() && !el2Iter.hasNext()) {
-            return false
-        }
-        if (!el1Iter.hasNext() && !el2Iter.hasNext()) {
-            return true
+            return -1
         }
 
         val item1 = el1Iter.next()
         val item2 = el2Iter.next()
-        if (!pairInRightOrder(item1, item2)) {
-            return false
+        val comparison = pairInRightOrder(item1, item2)
+        if (comparison != 0) {
+            return comparison
         }
     }
 
-
-    return true
-}
-
-fun isListOfIntHolder(el: EmbeddedList): Boolean {
-    return el.list.all { it is IntHolder }
+    return if (el1.list.size == el2.list.size) 0 else 1
 }
 
 fun getListElem(root: EmbeddedList, inputStr: String) {
@@ -134,31 +111,12 @@ fun getListElem(root: EmbeddedList, inputStr: String) {
 
     if (!inp.startsWith("[") && inp.contains("[")) {
         root.list.addAll(inp.substringBefore(",[").split(",").map { IntHolder(it.toInt()) })
-        inp = inp.substringAfter(",")
+        inp = inp.substring(inp.indexOf('['), inp.length)
     }
 
     if (inp.startsWith("[")) {
         val embedded = EmbeddedList()
-        val closingBracketIndex: Int
-        val heap: Deque<Char> = LinkedList()
-        heap.add('[')
-
-
-        val iter = inp.iterator()
-        var index = 1
-        iter.next() // skip 1st element
-        while (heap.isNotEmpty()) {
-            val next = iter.next()
-            if (next == '[') {
-                heap.add('[')
-            }
-            if (next == ']') {
-                heap.pop()
-            }
-            index++
-        }
-        closingBracketIndex = index - 1
-
+        val closingBracketIndex = getClosingBracketIndex(inp)
 
         val embeddedSubList = inp.substring(1, closingBracketIndex)
         getListElem(embedded, embeddedSubList)
@@ -166,6 +124,29 @@ fun getListElem(root: EmbeddedList, inputStr: String) {
         getListElem(root, inp.substring(closingBracketIndex, inp.length).substringAfter(',', missingDelimiterValue = ""))
         return
     }
+}
+
+private fun getClosingBracketIndex(inp: String): Int {
+    val closingBracketIndex: Int
+    val heap: Deque<Char> = LinkedList()
+    heap.add('[')
+
+
+    val iter = inp.iterator()
+    var index = 1
+    iter.next() // skip 1st element
+    while (heap.isNotEmpty()) {
+        val next = iter.next()
+        if (next == '[') {
+            heap.add('[')
+        }
+        if (next == ']') {
+            heap.pop()
+        }
+        index++
+    }
+    closingBracketIndex = index - 1
+    return closingBracketIndex
 }
 
 
